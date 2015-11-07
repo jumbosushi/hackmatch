d34c2d5b9e006d4caff83f1ad99aa8b3825db696
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-07 10:16:48 -0800

    this woks?

---
+++ main.js
@@ -0,0 +1,15 @@
+var user_email = document.getElementById("inputEmail");
+
+$("#submit").click(function() {
+    $.ajax({
+        type: "POST",
+        url: "https://hackmatch.azure-mobile.net/api/matchuser",
+        contentType: "application/json",
+        data: {
+                email: user_email;
+            },
+        success: function() {
+            console.log("it worked");
+        }
+    });
+});
