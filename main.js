f146b690f6f703ced0c2dd725bdc8f5a612e4d49
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-07 11:04:33 -0800

    fixed index bugs

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var user_email = document.getElementById("inputEmail");
+console.log(user_email);
 
 $("#submit").click(function() {
+    var user_email = document.getElementById("inputEmail");
+    console.log(user_email);
     $.ajax({
         type: "POST",
         url: "https://hackmatch.azure-mobile.net/api/matchuser",
