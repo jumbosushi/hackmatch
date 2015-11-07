f4c10164cfe435040e65e2a07f9ca2d021281910
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-07 15:12:04 -0800

    Add variables for casual and noob

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-var user_email = "asdasd"; //document.getElementById("#inputEmail").value;
+var casual = $('#Q1-Y').is(':checked');
+var newComer = $('#Q2-Y').is(':checked');
+var platforms = function() {
+    var plats = [];
+    if ($('#mob').is(':checked')) {
+        platss.add("Mobile");
+    } else if ($('#web').is(':checked')) {
+        plats.add("Web App");
+    } else if ($('#har').is(':checked')) {
+        plats.add("Hardware");
+    }
+};
 
 $(document).ready(function() {
 $("#singlebutton").click(function() {
-        console.log(user_email);
+        console.log(casual);
         $.ajax({
             type: "POST",
             url: "https://hackmatch.azure-mobile.net/api/matchuser",
             data: {
-                    email: user_email
+
                 },
             success: function() {
                 console.log("it worked!");
@@ -15,3 +26,5 @@ $("#singlebutton").click(function() {
         });
     });
 });
+
+//User (first, last, casual, newComer, platforms, skillSet, lookingFor, bio)
