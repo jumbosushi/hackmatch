42ac94257e58ed26a0a9d2b29d9e9628bc6db029
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2015-11-07 16:21:55 -0800

    complete HTML parser

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
+var skill_set = ["Web", "Development", "Android", "iOS", "Hardware", "HTML/CSS", "JavaScript", "Python","Java", "C/C++", "PHP", "Objective-C", "C#", "Swift", "JSON", "Ruby", "XML", "Ajax", "Shell", "Processing", "Lua", "CoffeeScript", "Go", "MATLAB", "OpenGL", "R", "Groovy", "XAML", "Perl", "WebGL", "Applescript", "Scala", "GLSL", "JSP", "LaTeX"];
+
 var casual = $('#Q1-Y').is(':checked');
 var newComer = $('#Q2-Y').is(':checked');
 var platforms = function() {
     var plats = [];
     if ($('#mob').is(':checked')) {
-        platss.add("Mobile");
+        plats.add("Mobile");
     } else if ($('#web').is(':checked')) {
         plats.add("Web App");
     } else if ($('#har').is(':checked')) {
         plats.add("Hardware");
     }
+    return plats;
 };
+
+var skillset = function() {
+    var skills = [];
+    for (var i = 0; i <= 34; i++) {
+        var num = "checkboxes-" + i.toString;
+        if (($('#'+num).is(':checked'))) {
+            skills.add(skill_set[i]);
+        }
+    }
+};
+
+var skillset = function() {
+    var skills = [];
+    for (var i = 0; i <= 34; i++) {
+        var num = "-" + i.toString;
+        if (($('#'+num).is(':checked'))) {
+            skills.add(skill_set[i]);
+        }
+    }
+};
+
+var bio = document.getElementById("textinput").text();
 
 $(document).ready(function() {
 $("#singlebutton").click(function() {
@@ -18,7 +43,12 @@ $("#singlebutton").click(function() {
             type: "POST",
             url: "https://hackmatch.azure-mobile.net/api/matchuser",
             data: {
-
+                casual: casual,
+                newComer: newComer,
+                platforms: platforms,
+                skillSet: skillset,
+                lookingFor: skillset,
+                bio: bio
                 },
             success: function() {
                 console.log("it worked!");
@@ -28,3 +58,7 @@ $("#singlebutton").click(function() {
 });
 
 //User (first, last, casual, newComer, platforms, skillSet, lookingFor, bio)
+
+var skill_set = ["Web", "Development", "Android", "iOS", "Hardware", "HTML/CSS", "JavaScript", "Python","Java", "C/C++", "PHP", "Objective-C", "C#", "Swift", "JSON", "Ruby", "XML", "Ajax", "Shell", "Processing", "Lua", "CoffeeScript", "Go", "MATLAB", "OpenGL", "R", "Groovy", "XAML", "Perl", "WebGL", "Applescript", "Scala", "GLSL", "JSP", "LaTeX"];
+
+
